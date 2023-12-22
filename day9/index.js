a1fef7ff4fd634201e977d96403a28c2f72468ae
Fahmi Bnchi <bnchifahmi@gmail.com>
2023-12-22 21:35:24 +0300

    day 9 part 2

--- day9/index.js
+++ day9/index.js
@@ -1,5 +1,4 @@
 function solve(testInput) {
-
   let finalSum = 0
   for (let r = 0; r < testInput.length; r++) {
     const history = testInput[r].split(' ').map((value) => parseInt(value))
@@ -12,28 +11,22 @@ function solve(testInput) {
       for (let i = 0; i < values[row].length - 1; i++) {
         nextRow.push(values[row][i + 1] - values[row][i])
       }
-
       values.push(nextRow)
-
       row += 1
     }
 
 
-    let sum = 0
-    while (values.length) {
-      const currentArray = values.pop()
-      const current = currentArray[currentArray.length - 1]
-
-      sum += current
+    let val = 0
+    for (let r = values.length - 1; r > 0; r--) {
+      val = values[r - 1][0] - val
     }
 
-    finalSum += sum
+    finalSum += val
   }
 
   console.log(finalSum)
 
 }
-
 
 function allZeros(values) {
   for (let i = 0; i < values.length; i++) {
